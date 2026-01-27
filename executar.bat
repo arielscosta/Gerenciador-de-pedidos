10f995ef4eef8d50f3bd485363028a35ed6dac67
Ariel Costa <arielsantoscosta@gmail.com>
2026-01-26 21:05:57 -0300

    adição dos comentários

--- executar.bat
+++ executar.bat
@@ -1,3 +1,3 @@
-@echo off
-python gerenciador_pedidos.py
-pause
+@echo off "Impede que os comandos do window sejam exibidos na tela"
+python gerenciador_pedidos.py "Executa o script Python gerenciador_pedidos.py"
+pause "Mantém a janela do prompt de comando aberta até que uma tecla seja pressionada"
